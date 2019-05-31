13ecce637662aa91e9ef8b3737efb269e744b57a
Ben Martinez-Bateman <ben@alwaysblank.org>
2019-05-31 00:24:19 +0000

    Fix some vscode stuff, hopefully

--- home/.zsh/vscode.zsh
+++ home/.zsh/vscode.zsh
@@ -1,3 +1,5 @@
 # Set up remote for VS Code
 WINDOWS_USERNAME="ben"
 VSCODE_PATH="/mnt/c/Users/${WINDOWS_USERNAME}/AppData/Local/Programs/Microsoft VS Code Insiders/bin"
+
+export PATH=$PATH:/mnt/c/Windows/System32:${VSCODE_PATH}
